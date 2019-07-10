7c8906fbd265aed1e0820b87713cd732e20a3792
rayjclin <nturay0321@gmail.com>
2019-07-10 14:55:13 +0800

    Add HelloViewSet in views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
@@ -45,3 +46,18 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
